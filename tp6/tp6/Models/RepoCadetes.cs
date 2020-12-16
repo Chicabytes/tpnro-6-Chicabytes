9df99110d42703d55f1b5248047ef0657904f3b4
Chicabytes <62666993+Chicabytes@users.noreply.github.com>
2020-12-16 19:47:45 -0300

    2 cambios

--- tp6/tp6/Models/RepoCadetes.cs
+++ tp6/tp6/Models/RepoCadetes.cs
@@ -13,124 +13,84 @@ namespace tp6.Models
         public List<Cadete> GetAll()
         {
             List<Cadete> NCadetes = new List<Cadete>();
-            try
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "Select * from Cadetes;";
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
             {
-
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "Select * from Cadetes;";
-                SQLiteDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    var cad = new Cadete(Convert.ToInt32(reader["idCadete"]), reader["NombreCadete"].ToString(), reader["DireccionCadete"].ToString(), reader["TelefonoCadete"].ToString(), Convert.ToInt32(reader["TipoTransporte"]));
-                    NCadetes.Add(cad);
-                }
-                reader.Close();
-                return NCadetes;
+                var cad = new Cadete(Convert.ToInt32(reader["idCadete"]), reader["NombreCadete"].ToString(), reader["DireccionCadete"].ToString(), reader["TelefonoCadete"].ToString(), Convert.ToInt32(reader["TipoTransporte"]));
+                NCadetes.Add(cad);
             }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-                return NCadetes;
-            }
+            reader.Close();
+            return NCadetes;
 
         }
         public void Alta(Cadete _cad)
         {
-            try
-            {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "Insert Into Cadetes(NombreCadete, DireccionCadete, TelefonoCadete, TipoTransporte) values (@NombreCadete, @DireccionCadete, @TelefonoCadete, @TipoTransporte)";
-                command.Parameters.AddWithValue("@NombreCadete", _cad.Nombre);
-                command.Parameters.AddWithValue("@DireccionCadete", _cad.Direccion);
-                command.Parameters.AddWithValue("@TelefonoCadete", _cad.Telefono);
-                command.Parameters.AddWithValue("@TipoTransporte", _cad.TipoT);
-                command.ExecuteNonQuery();
-                conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-            }
-
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "Insert Into Cadetes(NombreCadete, DireccionCadete, TelefonoCadete, TipoTransporte) values (@NombreCadete, @DireccionCadete, @TelefonoCadete, @TipoTransporte)";
+            command.Parameters.AddWithValue("@NombreCadete", _cad.Nombre);
+            command.Parameters.AddWithValue("@DireccionCadete", _cad.Direccion);
+            command.Parameters.AddWithValue("@TelefonoCadete", _cad.Telefono);
+            command.Parameters.AddWithValue("@TipoTransporte", _cad.TipoT);
+            command.ExecuteNonQuery();
+            conexion.Close();
         }
         public void Baja(int ID)
         {
-            try
-            {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "DELETE FROM Cadetes WHERE idCadete = @_id";
-                command.Parameters.AddWithValue("@_id", ID);
-                command.ExecuteNonQuery();
-                conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-            }
-
-        }
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "DELETE FROM Cadetes WHERE idCadete = @_id";
+            command.Parameters.AddWithValue("@_id", ID);
+            command.ExecuteNonQuery();
+            conexion.Close();
+       }
         public void Modificar(Cadete cad)
         {
-            try
-            {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "UPDATE Cadetes SET NombreCadete = @Nombre, DireccionCadete = @Direccion, TelefonoCadete = @Telefono, TipoTransporte = @TipoT WHERE idCadete = @ID";
-                command.Parameters.AddWithValue("@ID", cad.Id);
-                command.Parameters.AddWithValue("@Nombre", cad.Nombre);
-                command.Parameters.AddWithValue("@Direccion", cad.Direccion);
-                command.Parameters.AddWithValue("@Telefono", cad.Telefono);
-                command.Parameters.AddWithValue("@TipoT", cad.TipoT);
-                command.ExecuteNonQuery();
-                conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-            }
-
-        }
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "UPDATE Cadetes SET NombreCadete = @Nombre, DireccionCadete = @Direccion, TelefonoCadete = @Telefono, TipoTransporte = @TipoT WHERE idCadete = @ID";
+            command.Parameters.AddWithValue("@ID", cad.Id);
+            command.Parameters.AddWithValue("@Nombre", cad.Nombre);
+            command.Parameters.AddWithValue("@Direccion", cad.Direccion);
+            command.Parameters.AddWithValue("@Telefono", cad.Telefono);
+            command.Parameters.AddWithValue("@TipoT", cad.TipoT);
+            command.ExecuteNonQuery();
+            conexion.Close();
+       }
 
         public Cadete Buscar(int _id)
         {
             Cadete Cad = new Cadete();
-            try
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "Select * from Cadetes where idCadete = @_id;";
+            command.Parameters.AddWithValue("@_id", _id);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
             {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "Select * from Cadetes where idCadete = @_id;";
-                command.Parameters.AddWithValue("@_id", _id);
-                SQLiteDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    Cad.Id = Convert.ToInt32(reader["idCadete"]);
-                    Cad.Nombre = reader["NombreCadete"].ToString();
-                    Cad.Direccion = reader["DireccionCadete"].ToString();
-                    Cad.Telefono = reader["TelefonoCadete"].ToString();
-                    Cad.TipoT = (TipoTransporte)Convert.ToInt32(reader["TipoTransporte"]);
-                }
-                reader.Close();
-                conexion.Close();
-                return Cad;
+                Cad.Id = Convert.ToInt32(reader["idCadete"]);
+                Cad.Nombre = reader["NombreCadete"].ToString();
+                Cad.Direccion = reader["DireccionCadete"].ToString();
+                Cad.Telefono = reader["TelefonoCadete"].ToString();
+                Cad.TipoT = (TipoTransporte)Convert.ToInt32(reader["TipoTransporte"]);
             }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-                return Cad;
-            }
+            reader.Close();
+            conexion.Close();
+            return Cad;
+            
         }
     }
 }
--- tp6/tp6/Models/RepoClientes.cs
+++ tp6/tp6/Models/RepoClientes.cs
@@ -12,128 +12,87 @@ namespace tp6.Models
         public List<Cliente> GetAll()
         {
             List<Cliente> NClientes = new List<Cliente>();
-            try
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "Select * from Clientes;";
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
             {
-                
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "Select * from Clientes;";
-                SQLiteDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    var cli = new Cliente(Convert.ToInt32(reader["idCliente"]), reader["NombreCliente"].ToString(), reader["DireccionCliente"].ToString(), reader["TelefonoCliente"].ToString(), Convert.ToBoolean(reader["Cupon"]), Convert.ToDouble(reader["CostoTotal"]));
-                    NClientes.Add(cli);
-                }
-                reader.Close();
-                return NClientes;
+                var cli = new Cliente(Convert.ToInt32(reader["idCliente"]), reader["NombreCliente"].ToString(), reader["DireccionCliente"].ToString(), reader["TelefonoCliente"].ToString(), Convert.ToBoolean(reader["Cupon"]), Convert.ToDouble(reader["CostoTotal"]));
+                NClientes.Add(cli);
             }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-                return NClientes;
-            }
-            
+            reader.Close();
+            return NClientes;
         }
         public void Alta(Cliente Cli)
         {
-            try
-            {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "Insert Into Clientes(NombreCliente, DireccionCliente, TelefonoCliente, Cupon, CostoTotal) values (@NombreCliente, @DireccionCliente, @TelefonoCliente, @Cupon, @CostoTotal)";
-                command.Parameters.AddWithValue("@NombreCliente", Cli.Nombre);
-                command.Parameters.AddWithValue("@DireccionCliente", Cli.Direccion);
-                command.Parameters.AddWithValue("@TelefonoCliente", Cli.Telefono);
-                command.Parameters.AddWithValue("@Cupon", Cli.Cupon);
-                command.Parameters.AddWithValue("@CostoTotal", Cli.Costo_total);
-                command.ExecuteNonQuery();
-                conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-            }
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "Insert Into Clientes(NombreCliente, DireccionCliente, TelefonoCliente, Cupon, CostoTotal) values (@NombreCliente, @DireccionCliente, @TelefonoCliente, @Cupon, @CostoTotal)";
+            command.Parameters.AddWithValue("@NombreCliente", Cli.Nombre);
+            command.Parameters.AddWithValue("@DireccionCliente", Cli.Direccion);
+            command.Parameters.AddWithValue("@TelefonoCliente", Cli.Telefono);
+            command.Parameters.AddWithValue("@Cupon", Cli.Cupon);
+            command.Parameters.AddWithValue("@CostoTotal", Cli.Costo_total);
+            command.ExecuteNonQuery();
+            conexion.Close();
         }
 
         public void Baja(int _id)
         {
-            try
-            {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "DELETE FROM Clientes WHERE idCliente = @_id";
-                command.Parameters.AddWithValue("@_id", _id);
-                command.ExecuteNonQuery();
-                conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-            }
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "DELETE FROM Clientes WHERE idCliente = @_id";
+            command.Parameters.AddWithValue("@_id", _id);
+            command.ExecuteNonQuery();
+            conexion.Close();
             
         }
 
         public Cliente Buscar(int _id)
         {
             var Cli = new Cliente();
-            try
-            {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "Select * from Clientes where idCliente = @_id;";
-                command.Parameters.AddWithValue("@_id", _id);
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "Select * from Clientes where idCliente = @_id;";
+            command.Parameters.AddWithValue("@_id", _id);
                 
-                SQLiteDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-
-                    Cli.Id = Convert.ToInt32(reader["idCliente"]);
-                    Cli.Nombre = reader["NombreCliente"].ToString();
-                    Cli.Direccion = reader["DireccionCliente"].ToString();
-                    Cli.Telefono = reader["TelefonoCliente"].ToString();
-                    Cli.Cupon = Convert.ToBoolean(reader["Cupon"]);
-                    Cli.Costo_total = Convert.ToDouble(reader["CostoTotal"]);
-                }
-                return Cli;
-            }
-            catch (Exception ex)
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
             {
-                string excep = ex.ToString();
-                return Cli;
+
+                Cli.Id = Convert.ToInt32(reader["idCliente"]);
+                Cli.Nombre = reader["NombreCliente"].ToString();
+                Cli.Direccion = reader["DireccionCliente"].ToString();
+                Cli.Telefono = reader["TelefonoCliente"].ToString();
+                Cli.Cupon = Convert.ToBoolean(reader["Cupon"]);
+                Cli.Costo_total = Convert.ToDouble(reader["CostoTotal"]);
             }
-            
+            return Cli;
         }
         public void Modificar(Cliente Cli)
         {
-            try
-            {
-                string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
-                var conexion = new SQLiteConnection(cadena);
-                conexion.Open();
-                var command = conexion.CreateCommand();
-                command.CommandText = "UPDATE Clientes SET NombreCliente = @Nombre, DireccionCliente = @Direccion, TelefonoCliente = @Telefono, Cupon = @Cupon, CostoTotal = @CostoTotal WHERE idCliente = @ID";
-                command.Parameters.AddWithValue("@ID", Cli.Id);
-                command.Parameters.AddWithValue("@Nombre", Cli.Nombre);
-                command.Parameters.AddWithValue("@Direccion", Cli.Direccion);
-                command.Parameters.AddWithValue("@Telefono", Cli.Telefono);
-                command.Parameters.AddWithValue("@Cupon", Cli.Cupon);
-                command.Parameters.AddWithValue("@CostoTotal", Cli.Costo_total);
-                command.ExecuteNonQuery();
-                conexion.Close();
-            }
-            catch (Exception ex)
-            {
-                string excep = ex.ToString();
-            }
-            
+            string cadena = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "Data\\tp6.db");
+            var conexion = new SQLiteConnection(cadena);
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = "UPDATE Clientes SET NombreCliente = @Nombre, DireccionCliente = @Direccion, TelefonoCliente = @Telefono, Cupon = @Cupon, CostoTotal = @CostoTotal WHERE idCliente = @ID";
+            command.Parameters.AddWithValue("@ID", Cli.Id);
+            command.Parameters.AddWithValue("@Nombre", Cli.Nombre);
+            command.Parameters.AddWithValue("@Direccion", Cli.Direccion);
+            command.Parameters.AddWithValue("@Telefono", Cli.Telefono);
+            command.Parameters.AddWithValue("@Cupon", Cli.Cupon);
+            command.Parameters.AddWithValue("@CostoTotal", Cli.Costo_total);
+            command.ExecuteNonQuery();
+            conexion.Close();
         }
     }
 }
